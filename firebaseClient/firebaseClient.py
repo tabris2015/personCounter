c3c6ab0dc56ca62fa4337d4c0d38df7dd3ae1f34
Jose Laruta <eduardo.laruta@gmail.com>
2018-05-31 19:16:00 -0400

    funciona con firestore

--- firebaseClient/firebaseClient.py
+++ firebaseClient/firebaseClient.py
@@ -6,32 +6,64 @@ from firebase import firebase
 import sqlite3
 from datetime import datetime, timedelta
 
+
+#///////////////////////////////////////////
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 #/////////////////////////////////////////////////
 
 events = []
-
 events2 = []
 ####
 connected = False
 
-def handle_data(data):
+def handle_data_s1(data):
     #print(type(data), data)
     #print("code: ", data[0])
     event_dic = {}
     code = data[0]
     global events
     if code == 'I':
-        event_dic["type"] = 'IN'
-        event_dic["tstamp"] = datetime.now()
-        events2.append((datetime.now(), 'IN'))
-        print("in")
+        event_dic["tipo_marcado"] = 1
+        event_dic["fecha"] = datetime.utcnow()
+        event_dic["id_sensor"] = 1
+        events2.append((datetime.now(), 1))
+        print("in1")
         events.append(event_dic)
         #first_event = True
     elif code == 'O':
-        event_dic["type"] = 'OUT'
-        event_dic["tstamp"] = datetime.now()
-        events2.append((datetime.now(), 'OUT'))
-        print("out")
+        event_dic["tipo_marcado"] = 0
+        event_dic["fecha"] = datetime.utcnow()
+        event_dic["id_sensor"] = 1
+        events2.append((datetime.now(), 0))
+        print("out1")
+        #first_event = True
+        events.append(event_dic)
+    else:
+        pass
+        #print("no code")
+
+def handle_data_s2(data):
+    #print(type(data), data)
+    #print("code: ", data[0])
+    event_dic = {}
+    code = data[0]
+    global events
+    if code == 'I':
+        event_dic["tipo_marcado"] = 1
+        event_dic["fecha"] = datetime.utcnow()
+        event_dic["id_sensor"] = 2
+        events2.append((datetime.now(), 1))
+        print("in2")
+        events.append(event_dic)
+        #first_event = True
+    elif code == 'O':
+        event_dic["tipo_marcado"] = 0
+        event_dic["fecha"] = datetime.utcnow()
+        event_dic["id_sensor"] = 2
+        events2.append((datetime.now(), 0))
+        print("out2")
         #first_event = True
         events.append(event_dic)
     else:
@@ -40,8 +72,9 @@ def handle_data(data):
 
 
 
-def read_from_port(ser):
+def read_from_port1(ser):
     global connected
+    print("hilo serial 1")
     while not connected:
         connected = True
 
@@ -51,18 +84,36 @@ def read_from_port(ser):
                 reading = ser.readline()
                 ser.flush()
                 if reading:
-                    handle_data(reading)
+                    handle_data_s1(reading)
+
+                time.sleep(0.05)
+
+
+def read_from_port2(ser):
+    global connected
+    print("hilo serial 2")
+    while not connected:
+        connected = True
+
+        while True:
+            #print("test")
+            if ser.in_waiting:
+                reading = ser.readline()
+                ser.flush()
+                if reading:
+                    handle_data_s2(reading)
 
                 time.sleep(0.05)
 
 
 
-
-def pushToLocalDB(db, event, firebase=False):
+def pushToLocalDB(db, event):
     insert_SQL = '''INSERT INTO personEvent(tstamp, type) VALUES(?, ?)'''
     c = db.cursor()
     c.execute(insert_SQL,(event['tstamp'], event['type']))
     db.commit()
+
+
 
 def select_all_events(conn):
     """
@@ -96,7 +147,12 @@ def select_last_events(conn):
  
 
 def periodicDBInsert():
+    #///////////////////
+    cred = credentials.Certificate('key2.json')
+    firebase_admin.initialize_app(cred)
+    dbFs = firestore.client()
     # for sqlite
+    
     insert_SQL = '''INSERT INTO personEvent(tstamp, type) VALUES(?, ?)'''
     global events
     global events2
@@ -110,6 +166,14 @@ def periodicDBInsert():
             print("insertando eventos...")
             # for event in events:
             #     pushToLocalDB(db, event)
+            # creando doc
+            doc_ref = dbFs.collection(u'marcados_eventos').document(unicode(datetime.now()))
+            doc_data = {
+                            'marcados':events,
+                            'id_evento': 1,
+
+            }   
+            doc_ref.set(doc_data)
 
             c.executemany(insert_SQL, events2)
             db.commit()
@@ -117,24 +181,34 @@ def periodicDBInsert():
             events = []
             events2 = []
 
-        time.sleep(10)
+        time.sleep(30)
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='contador de personas')
-    parser.add_argument('-serial', action='store',help='for serial port')
+    parser.add_argument('-serial1', required=True, action='store',help='for serial port1')
+    parser.add_argument('-serial2', required=True, action='store',help='for serial port2')
 
     args = parser.parse_args()
 
-    if args.serial != None:
+    if args.serial1 != None and args.serial2 != None:
         print("puerto serial!")
-        port = args.serial
+        port1 = args.serial1
+        port2 = args.serial2
         baud = 115200
-        serial_port = serial.Serial(port, baud, timeout=0)
-        serialTh = threading.Thread(target=read_from_port, args=(serial_port,))
-        serialTh.daemon = True
-        serialTh.start()
+
+        serial_port1 = serial.Serial(port1, baud, timeout=0)
+        print("serial1 conectado!")
+        serial_port2 = serial.Serial(port2, baud, timeout=0)
+        print("serial2 conectado!")
+        
+        serialTh1 = threading.Thread(target=read_from_port1, args=(serial_port1,))
+        serialTh2 = threading.Thread(target=read_from_port2, args=(serial_port2,))
+        serialTh1.daemon = True
+        serialTh2.daemon = True
+        serialTh1.start()
+        serialTh2.start()
     else:
         port = "/dev/ttyUSB0"
 
--- firebaseClient/testFB.py
+++ firebaseClient/testFB.py
@@ -2,6 +2,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from datetime import datetime
 
 # Use the application default credentials
 cred = credentials.Certificate('key2.json')
@@ -15,3 +16,15 @@ docs = eventos_ref.get()
 
 for doc in docs:
     print(u'{} => {}'.format(doc.id, doc.to_dict()))
+
+
+
+
+doc_ref = db.collection(u'marcados_eventos').document(u'pepe2')
+
+data = {'id_evento': 1,
+        'marcados': [{'fecha':datetime.now(), 'tipo_marcado':1}]}
+
+doc_ref.set(data)
+
+

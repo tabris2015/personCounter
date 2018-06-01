1ff04f76e9660d3b5ed0238a6bbaf805054221d9
Jose Laruta <eduardo.laruta@gmail.com>
2018-05-31 23:46:07 -0400

    con funciones

--- firebaseClient/gpioTest.py
+++ firebaseClient/gpioTest.py
@@ -1,6 +1,6 @@
 
 from gpiozero import Button
-
+from time import sleep
 
 ##### pin definitions
 
@@ -8,12 +8,6 @@ IN1 = 5
 OUT1 = 6
 IN2 = 13
 OUT2 = 19
-
-in1_button = Button(IN1, pull_up=False)
-out1_button = Button(OUT1, pull_up=False)
-in2_button = Button(IN2, pull_up=False)
-out2_button = Button(OUT2, pull_up=False)
-
 
 def in1():
     print("in1!")
@@ -28,5 +22,17 @@ def out2():
     print("out2!")
 
 
+in1_button = Button(IN1, pull_up=False)
+out1_button = Button(OUT1, pull_up=False)
+in2_button = Button(IN2, pull_up=False)
+out2_button = Button(OUT2, pull_up=False)
+
+
+in1_button.when_pressed = in1
+out1_button.when_pressed = out1
+in2_button.when_pressed = in2
+out2_button.when_pressed = out2
+
+
 while True:
-    pass
+    sleep(0.2)
